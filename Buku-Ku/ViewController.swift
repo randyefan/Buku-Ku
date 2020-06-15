f568445542196c763b2e3d2c2d9d78190866cbb8
Randy Efan <randyrandyrej8@gmail.com>
2020-06-15 11:27:20 +0700

    Initial Commit

---
+++ Buku-Ku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buku-Ku
+//
+//  Created by Randy Efan Jayaputra on 15/06/20.
+//  Copyright © 2020 Randy Efan Jayaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
